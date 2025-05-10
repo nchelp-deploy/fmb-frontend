a260caebbc8a8502abe3a09acc928c36fcf7c9ff
nchelp-deploy <netcyberhelp@gmail.com>
2025-05-10 14:47:32 +0100

    Add files via upload

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import api from '../utils/axiosConfig';
 import Loading from '../components/Loading';
@@ -17,6 +17,10 @@ function Signup() {
   const [isLoading, setIsLoading] = useState(false);
   const [successMessage, setSuccessMessage] = useState('');
   const navigate = useNavigate();
+
+  useEffect(() => {
+    console.log('Signup page loaded');
+  }, []);
 
   const handleChange = (e) => {
     const { name, value } = e.target;
